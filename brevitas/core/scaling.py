19045dde15a52a41ae89181a13ac99ef10f2f0a6
Alessandro Pappalardo <alessand@xilinx.com>
2020-02-18 18:12:05 +0000

    Fix lookup of state dict value only when key exist
    Signed-off-by: Alessandro Pappalardo <alessand@xilinx.com>

--- brevitas/core/scaling.py
+++ brevitas/core/scaling.py
@@ -115,7 +115,9 @@ class StandaloneScaling(torch.jit.ScriptModule):
         if retrocomp_value_key in state_dict: #  Retrocompatibility
             state_dict[value_key] = state_dict.pop(retrocomp_value_key)
         # convert per channel to per tensor with a mean
-        if state_dict[value_key].shape != SCALING_SCALAR_SHAPE and self.learned_value.shape == SCALING_SCALAR_SHAPE:
+        if value_key in state_dict and \
+                state_dict[value_key].shape != SCALING_SCALAR_SHAPE and \
+                self.learned_value.shape == SCALING_SCALAR_SHAPE:
             state_dict[value_key] = state_dict[value_key].mean()
         super(StandaloneScaling, self)._load_from_state_dict(state_dict, prefix, local_metadata, strict,
             missing_keys, unexpected_keys, error_msgs)
