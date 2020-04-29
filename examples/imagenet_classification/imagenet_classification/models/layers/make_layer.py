b3f489b41ec95d2537a12e9a7c7ea8f45ed4529b
Alessandro Pappalardo <volcacius@users.noreply.github.com>
2020-04-29 12:56:07 +0100

    Add missing abs call in top10ave
    Signed-off-by: Alessandro Pappalardo <volcacius@users.noreply.github.com>

--- examples/imagenet_classification/imagenet_classification/models/layers/make_layer.py
+++ examples/imagenet_classification/imagenet_classification/models/layers/make_layer.py
@@ -81,7 +81,7 @@ class BatchTop10AveNorm2d(torch.jit.ScriptModule):
         batchsize, channels, height, width = input_.size()
         permuted_input_ = input_.permute(1, 0, 2, 3).contiguous().view(channels, -1)
         if self.training:
-            top10_ave = permuted_input_.topk(k=10, dim=1, sorted=False, largest=True)[0].mean(dim=1)
+            top10_ave = permuted_input_.abs().topk(k=10, dim=1, sorted=False, largest=True)[0].mean(dim=1)
             if self.first:
                 self.running_top10_ave = top10_ave.detach()
                 self.first = False
